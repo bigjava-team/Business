7da143cf36aaebc09dc093364c10be54b95daec0
cx778 <47965403+cx778@users.noreply.github.com>
2019-04-08 10:43:36 +0800

    commit

--- src/org/bigjava/orderitem/action/OrderItemAction.java
+++ src/org/bigjava/orderitem/action/OrderItemAction.java
@@ -2,6 +2,7 @@ package org.bigjava.orderitem.action;
 
 import java.util.List;
 
+import org.bigjava.function.FileImageAction;
 import org.bigjava.function.IsEmpty;
 import org.bigjava.function.Paging;
 import org.bigjava.orderitem.biz.OrderItemBiz;
@@ -21,6 +22,7 @@ public class OrderItemAction {
 	private Paging paging;
 	
 	private IsEmpty isEmpty;
+	private FileImageAction fileImageAction;
 	
 	private OrderItemBiz orderItemBiz;
 	private UserBiz userBiz;
@@ -28,6 +30,14 @@ public class OrderItemAction {
 	
 	private List<Orderitem> listOrderitem;
 	
+	public FileImageAction getFileImageAction() {
+		return fileImageAction;
+	}
+
+	public void setFileImageAction(FileImageAction fileImageAction) {
+		this.fileImageAction = fileImageAction;
+	}
+
 	public ProductBiz getProductBiz() {
 		return productBiz;
 	}
@@ -88,18 +98,19 @@ public class OrderItemAction {
 		this.orderItemBiz = orderItemBiz;
 	}
 	
+	// 添加入购物车
 	public String addOrderItemAndQueryOrderItem() {
 		System.out.println("开始添加进购物车");
 		System.out.println(loginUser);
 		System.out.println(product);
 		System.out.println(orderitem);
-		if (loginUser == null || loginUser.equals("")) {
+		if (loginUser.getUsername() == null || loginUser.getUsername().equals("")) {
 			System.out.println("请先登录账户");
 			return "loginError";
 		}
 		loginUser = userBiz.queryUsernameUser(loginUser.getUsername());
 		product = productBiz.queryProduct_id(product.getP_id());
-		int count = orderitem.getCount();
+		int count = orderitem.getCount(); 
 		// 添加用于的外键，商品的外键
 		orderitem = new Orderitem(loginUser, product, null);
 		// 添加商品的价格
@@ -111,10 +122,24 @@ public class OrderItemAction {
 		int number = orderItemBiz.queryOrderItemNumber(loginUser);
 		paging = new Paging(paging.getPresentPage(), number, 10);
 		
+		// 查询的订单内容
 		listOrderitem = orderItemBiz.queryAllOrderItem(loginUser, paging);
 		
 		return "addOrderItemAndQueryOrderItem";
 	}
 	
-	
+	// 查看购物车
+	public String queryUserOrderitem() {
+		if (loginUser.getUsername() == null || loginUser.getUsername().equals("")) {
+			System.out.println("请先登录账户");
+			return "loginError";
+		}
+		loginUser = userBiz.queryUsernameUser(loginUser.getUsername());
+		int number = orderItemBiz.queryOrderItemNumber(loginUser);
+		paging = new Paging(paging.getPresentPage(), number, 10);
+		
+		// 查询的订单内容
+		listOrderitem = orderItemBiz.queryAllOrderItem(loginUser, paging);
+		return "queryUserOrderitem";
+	}
 }
